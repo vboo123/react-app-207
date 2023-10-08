85fa448d4897712754373bdf5cf5e6f8191cfb05
Vibhu Garg <vibhugarg@Vibhus-MacBook-Pro.local>
2023-10-08 14:22:02 -0700

    added fetch

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,9 +1,25 @@
 // src/MyApp.js
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
+//useEffect hook is a way to run code on every render
+
 function MyApp() {
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+}
+
+useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+}, [] );
+//We indicated that our useEffect hook should be called only when the MyApp component first mounts by passing an empty array []
+//the empty array indiciates that we want to trigger this if the state is emptys
   //useState returns a pair
   //current state value
   //a function that lets you update it
